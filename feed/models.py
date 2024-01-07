f358fe83054b95b6de7a0776ec63a556f92c8c35
EndaMagennis <>
2024-01-07 21:03:25 +0000

    Create basic models, views and templates for viewing posts

--- feed/models.py
+++ feed/models.py
@@ -14,11 +14,12 @@ GENRES = [
     ("rpg", "Role-Playing Game"),
     ("strat", "Strategy"),
     ("war", "Wargame"),
-    ("oth", "Other. Please specify")
+    ("oth", "Other")
 ]
 
 # Constant to hold the player count choices of game genre
 PLAYER_COUNT = [
+    ("any", "Any"),
     ("one_to_two", "1-2 Players"),
     ("one_to_four", "1-4 Players"),
     ("one_to_six", "1-6 Players"),
@@ -26,16 +27,20 @@ PLAYER_COUNT = [
     ("two_to_six", "2-6 Players"),
     ("two_to_eight", "2-8 Players"),
     ("two_to_fifteen", "2-15 Players"),
-    ("oth", "Other. Please specify")
+    ("two_to_six", "3-6 Players"),
+    ("two_to_eight", "3-8 Players"),
+    ("two_to_fifteen", "3-15 Players"),
+    ("oth", "Other")
 ]
 
 # Constant to hold the age range choices of game genre
 AGE_RANGE = [
+    ("all", "All ages"),
     ("four_plus", "4 +"),
     ("twelve_plus", "12 +"),
     ("fifteen_pus", "15 +"),
     ("eigtheen_plus", "18 +"),
-    ("oth", "Other. Please specify")
+    ("oth", "Other")
 ]
 
 # Constant to hold the play time choices of game genre
@@ -45,7 +50,7 @@ PLAY_TIME = [
     ("two_to_four", "2-4 hours"),
     ("over_four", "4 + hours"),
     ("rest_of", "The rest of your life"),
-    ("oth", "Other. Please specify")
+    ("oth", "Other")
 ]
 
 # Constant to hold the experience choices of game genre
@@ -86,7 +91,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     genre = models.CharField(max_length=20, choices=GENRES, default="camp")
     number_of_players = models.CharField(
-        max_length=20, choices=PLAYER_COUNT, default="one_to_four")
+        max_length=100, choices=PLAYER_COUNT, default="one_to_four")
     age_range = models.CharField(
         max_length=20, choices=AGE_RANGE, default="four_plus")
     play_time = models.CharField(
@@ -94,3 +99,17 @@ class Post(models.Model):
     experience = models.IntegerField(choices=EXPERIENCE, default=1)
     price_range = models.IntegerField(choices=PRICE_RANGE, default=1)
     rating = models.IntegerField(choices=RATING, default=1)
+    number_of_likes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+class Like(models.Model):
+    post_to_like = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="liked_post")
+    user_who_liked = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_who_liked")
--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.contrib import messages
+from .models import Post
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "feed/index.html"
+    paginate_by = 10
+
+
+def post_detail(request, slug):
+
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "feed/post_detail.html",
+        {
+            "post": post,
+        },
+    )
