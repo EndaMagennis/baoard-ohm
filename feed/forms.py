c93acb0e1dfcf9ac652636ced5da94e0916d8c22
Enda <endamagennis@gmail.com>
2024-01-10 22:26:07 +0000

    Remove author option from form

--- feed/forms.py
+++ feed/forms.py
@@ -3,9 +3,10 @@ from django import forms
 
 
 class PostForm(forms.ModelForm):
+    '''Form to create a new post'''
     class Meta:
         model = Post
-        fields = ('title', 'author', 'content',
+        fields = ('title', 'content',
                   'genre', 'number_of_players', 'age_range',
                   'play_time', 'experience', 'price_range',
                   'rating', )
