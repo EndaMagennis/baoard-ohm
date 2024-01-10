53d55b9501e11dd5a9d0e4cd70591451f5cbb53c
Enda <endamagennis@gmail.com>
2024-01-10 22:27:11 +0000

    Add paths for Creating Updating and Deleting posts

--- feed/urls.py
+++ feed/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('post_create/', views.AddPost.as_view(), name='post_create'),
+    path('<slug:slug>/', views.PostRead.as_view(), name='post_detail'),
+    path('<slug:slug>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('<slug:slug>/delete/', views.PostDelete.as_view(), name='post_delete'),
+
 ]
