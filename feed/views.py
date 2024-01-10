c2c791740843803755747e97f9604f0c53736c78
Enda <endamagennis@gmail.com>
2024-01-10 22:28:12 +0000

    Create basic class based views for CRUD functionality

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from typing import Any
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic, View
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
@@ -8,27 +9,68 @@ from .forms import PostForm
 
 
 class PostList(generic.ListView):
+    ''' List all posts '''
+
     queryset = Post.objects.all()
     template_name = "feed/index.html"
     paginate_by = 10
 
 
-def post_detail(request, slug):
+class AddPost(View):
+    '''Add a new post'''
 
-    queryset = Post.objects.all()
-    post = get_object_or_404(queryset, slug=slug)
+    def get(self, request: Any) -> Any:
+        form = PostForm()
+        return render(request, "feed/post_create.html", {"form": form})
 
-    if request.method == 'POST':
-        post_form = PostForm(data=request.POST)
-        if post_form.is_valid():
-            post = post_form.save(commit=False)
+    def post(self, request: Any) -> Any:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.slug = post.title.replace(" ", "-")
             post.save()
+            messages.add_message(request, messages.SUCCESS, "Post created successfully")
+            return redirect("home")
+        else:
+            messages.add_message(request, messages.ERROR, "Post creation failed")
+            return render(request, "feed/post_create.html", {"form": form})
 
-    post_form = PostForm()
-    return render(
-        request,
-        "feed/post_detail.html",
-        {
-            "post": post,
-        },
-    )
+class PostRead(generic.DetailView):
+    '''Read a post'''
+
+    model = Post
+    template_name = "feed/post_read.html"
+    
+class PostUpdate(generic.UpdateView):
+    '''Update a post'''
+
+    model = Post
+    template_name = "feed/post_update.html"
+    form_class = PostForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        messages.success(self.request, "Post updated successfully")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "Post update failed")
+        return super().form_invalid(form)
+    
+class PostDelete(generic.DeleteView):
+    '''Delete a post'''
+    
+    model = Post
+    template_name = "feed/post_delete.html"
+    success_url = "/"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Post deleted successfully")
+        return super().delete(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        messages.error(self.request, "Post deletion failed")
+        return super().post(request, *args, **kwargs)
+    
+
