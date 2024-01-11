9eb0caa4cda29c2bcc113b411d02bcda909c50b6
Enda <endamagennis@gmail.com>
2024-01-11 15:50:02 +0000

    Add static files to paths

--- board_ohm/urls.py
+++ board_ohm/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
@@ -26,3 +28,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('feed.urls'), name='feed-urls'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
