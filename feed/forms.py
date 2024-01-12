927596a14b524bbd2e78f7ab053a61de05bc3d0b
Enda <endamagennis@gmail.com>
2024-01-12 19:27:54 +0000

    Enhance UX with updated form label

--- feed/forms.py
+++ feed/forms.py
@@ -11,3 +11,28 @@ class PostForm(forms.ModelForm):
                   'genre', 'number_of_players', 'age_range',
                   'play_time', 'experience', 'price_range',
                   'rating', )
+        labels = {
+            'title': 'Title',
+            'content': 'Content',
+            'image': 'Image (If no image is selected, a default image will be used)',
+            'genre': 'Genre',
+            'number_of_players': 'Number of Players',
+            'age_range': 'Age Range',
+            'play_time': 'Play Time',
+            'experience': 'Experience',
+            'price_range': 'Price Range',
+            'rating': 'Rating',
+        }
+        
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control',}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control',}),
+            'genre': forms.Select(attrs={'class': 'form-control'}),
+            'number_of_players': forms.Select(attrs={'class': 'form-control'}),
+            'age_range': forms.Select(attrs={'class': 'form-control'}),
+            'play_time': forms.Select(attrs={'class': 'form-control'}),
+            'experience': forms.Select(attrs={'class': 'form-control'}),
+            'price_range': forms.Select(attrs={'class': 'form-control'}),
+            'rating': forms.Select(attrs={'class': 'form-control'}),
+        }
